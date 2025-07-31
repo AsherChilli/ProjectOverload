284c73b5f4a0ba6b39914ec1640eb5857f6af149
Username-not-used <lasteward@icloud.com>
2025-07-31 12:22:30 -0500

    Update SwerveDrivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDrivetrain.java
@@ -1,18 +1,50 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.HardwareDevice;
 import com.qualcomm.robotcore.hardware.Servo;
-
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+import com.pedropathing.follower.Follower;
 import java.util.ArrayList;
-import java.util.LinkedList;
 
-public class SwerveDrivetrain extends DrivetrainSubsystem {
+
+@TeleOp(name = "SwerveDrive", group = "Driving")
+@Disabled
+public class SwerveDrivetrain extends OpMode {
   private ArrayList<PDFLController> PDFLController;
   
   public SwerveDrivetrain(ArrayList<PDFLController> PDFLController, ArrayList<DcMotorEx> motors, ArrayList<Servo> servos, double[] position) {
-    super(motors, servos, position); 
     this.PDFLController = PDFLController;
     
   }
+
+  public static boolean robotCentric = false;
+
+    public void init() {
+      telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
+      follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
+    }
+
+    @Override
+    public void init_loop() {
+      //INIT LOOP
+      //We never put anything here for motors/servos
+      //as its against the rules for the bots to move AT ALL
+      //in the initialization phase of the Driver Controlled Period.
+      //For testing purposes, obviously yes you can put stuff here.
+
+
+      telemetry.addLine("Initialized!");
+      telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
